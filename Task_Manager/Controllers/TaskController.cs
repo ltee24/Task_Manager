a06ca8856e1f96500aa8a2d921212b9f1cd9146f
AbdullateefJokanola <ayomidejokanola08@gmail.com>
2025-12-06 13:19:35 +0100

    Add project files.

---
+++ Task_Manager/Controllers/TaskController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task_Manager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
